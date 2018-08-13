be7b95bec753b6d25469b67fb801c5656ae490c4
Henry Haller <henrybhaller@gmail.com>
2018-08-13 17:45:32 +0900

    took out email from user

--- db/migrate/20180813083515_add_data_to_user.rb
+++ db/migrate/20180813083515_add_data_to_user.rb
@@ -1,6 +1,5 @@
 class AddDataToUser < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :email, :string
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
   end
