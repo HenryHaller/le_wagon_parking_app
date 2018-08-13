206518890d8599ded95f4a04e2327c161486593b
Henry Haller <henrybhaller@gmail.com>
2018-08-13 18:04:49 +0900

    fix numericality

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,7 +2,7 @@ class Car < ApplicationRecord
   belongs_to :user
   validates :make, presence: true
   validates :model, presence: true
-  validates :year, presence: true, numeralicality: { only_integer: true }
+  validates :year, presence: true, numericality: { only_integer: true }
   validates :user, presence: true
   validates :license_plate, presence: true
 end
