998180fb84c130a7e2cc56578645b282bb9712fa
Henry Haller <henrybhaller@gmail.com>
2018-08-14 17:16:31 +0900

    basic booking is working

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,17 +1,30 @@
 class BookingsController < ApplicationController
+
   def index
     @bookings = Booking.all
   end
 
   def new
     @booking = Booking.new
+    @spot = Spot.find(params[:spot_id])
+    @cars = Car.all
   end
 
   def create
-    raise
+    booking = Booking.new(booking_params)
+    booking.spot = Spot.find(params[:spot_id])
+    booking.car = Car.find(params[:booking][:car_id])
+    if booking.save
+      redirect_to bookings_path
+    end
   end
 
   def delete
     raise
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:duration)
   end
 end
