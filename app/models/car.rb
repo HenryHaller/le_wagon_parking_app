3556d10d5758eae9c9997c597df91338ec2b6aac
Henry Haller <henrybhaller@gmail.com>
2018-08-14 14:56:10 +0900

    add basic home controller

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,6 @@
 class Car < ApplicationRecord
   belongs_to :user
+  has_many :bookings
   validates :make, presence: true
   validates :model, presence: true
   validates :year, presence: true, numericality: { only_integer: true }
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,4 +1,5 @@
 class Spot < ApplicationRecord
   belongs_to :user
   validates :site_name, :address, :user, :location, :description, presence: true
+  has_many :bookings
 end
