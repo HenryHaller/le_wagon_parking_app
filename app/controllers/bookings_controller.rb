43378bc929bd3ec17ef1912dec7d65aeae3b2931
Henry Haller <henrybhaller@gmail.com>
2018-08-14 18:11:33 +0900

    books displays car bookings and spot bookings seperately

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,17 +1,17 @@
 class BookingsController < ApplicationController
 
   def index
-    @bookings = Booking.all
+    # @bookings = Booking.all
     @car_bookings = []
     @spot_bookings = []
     current_user.cars.each do |car|
       car.bookings.each do |booking|
-        @car_bookings += booking
+        @car_bookings << booking
       end
     end
     current_user.spots.each do |spot|
       spot.bookings.each do |booking|
-        @spot_bookings += booking
+        @spot_bookings << booking
       end
     end
   end
