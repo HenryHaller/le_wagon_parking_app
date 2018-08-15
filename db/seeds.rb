0aa82930ffc4d93df3b9cabbb8774d073cd19db0
Ableen Tai <ableen@gmail.com>
2018-08-15 14:59:49 +0900

    fixed layout of spots and car bookings columns

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,10 @@
 
 #Spot
 #User
-
+Booking.delete_all
 Spot.delete_all
 Car.delete_all
 User.delete_all
-Booking.delete_all
 
 
 #creating sample user
@@ -26,7 +25,7 @@ puts "Created #{User.count} Users"
 
 
 User.all.each do |user|
-  1.times do
+  3.times do
     Spot.create(site_name: "fake site name", address: "fake address", hourly_rate: (5..30).to_a.sample, description: 'some spot', location: "75n, 78s", user: user)
   end
 end
@@ -37,7 +36,7 @@ puts "created #{Spot.count} spots"
 
 User.all.each do |user|
 maker = ["https://www.popsci.com/sites/popsci.com/files/styles/1000_1x_/public/images/2018/03/senna.jpg?itok=eYNPMGjA&fc=50,50", "https://amp.businessinsider.com/images/5aabc7bbc72ac12f008b4609-750-563.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM3XKvQ4tbYVq4rEIvphrSncrNkf_J0_-ugWNgADpqYEtVNiywqg", "https://www.fitmycar.com/journal/wp-content/uploads/2015/12/Top-Ten-Import-Tuner-Cars.jpg", "https://images.pexels.com/photos/707046/pexels-photo-707046.jpeg?cs=srgb&dl=light-road-landscape-707046.jpg&fm=jpg"].sample
-1.times do
+2.times do
     maker_car = Car.new(make: "fake make", model: "fake model", license_plate: "fake plate", color: 'a color', year: 1999, user: user)
     maker_car.remote_photo_url = maker
     maker_car.save
