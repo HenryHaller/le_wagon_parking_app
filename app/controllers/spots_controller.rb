9011bbdc5e9f72763c124319e20af4148fd19fc5
Henry Haller <henrybhaller@gmail.com>
2018-08-17 13:26:10 +0900

    filter by whether the spot exists

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -35,26 +35,57 @@ class SpotsController < ApplicationController
   end
 
   def results
-    @location = Geocoder.search(location)[0]
+    # get location if exists else nil. used only for sorting spots by distance
+    location = Geocoder.search(location)[0]
+    #get start and end time into a booking
     @booking = Booking.new(time_params)
+    @searching = true
+
+    #redirect to root if booking makes no sense
+    redirect_to root_path and return if @booking.begin.to_i >= @booking.end.to_i
     @cars = current_user.cars
     @car = Car.new
-    if @location
-      if current_user
-        @spots = Spot.where.not(user: current_user)
-      else
-        @spots = Spot.all
-      end
-      @spots.sort_by { |spot| spot.distance_from([@location.latitude, @location.longitude]) }
-      @markers = markers(@spots)
-      @searching = true
 
-      render :index
-    else
-      redirect_to root_path
+    @spots = Spot.all
+    #if we have a current user, reject out their spots
+    @spots = @spots.reject {|spot| spot.user == current_user} if user_signed_in?
+    #reject out spots that have bookings
+    @spots.select! {|spot| check_availible_spot(spot, @booking) }
+
+    # if we have a location, sort by it
+    if location
+      @spots.sort_by { |spot| spot.distance_from([@location.latitude, @location.longitude]) }
     end
+    @markers = markers(@spots)
+
+    render :index
   end
+  # else
+  #   redirect_to root_path
+  # end
+
   private
+
+  def check_availible_spot(spot, b1)
+    answer =  spot.bookings.none? { |booking| overlaps?(b1, booking) }
+    puts "#{answer} #{spot.site_name}"
+    return answer
+  end
+
+  def overlaps?(b1, b2)
+    b1begin = b1.begin.to_i
+    b2begin = b2.begin.to_i
+
+    b1end = b1.end.to_i
+    b2end = b2.end.to_i
+
+    return true if b1begin > b2begin && b1begin < b2end
+    return true if b1end > b2begin && b1end < b2end
+    return true if b1begin < b2begin && b1end > b2end
+    return false
+  end
+
+
   def time_params
     params.require(:results).permit(:begin, :end)
   end
